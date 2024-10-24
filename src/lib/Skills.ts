7edd1098eec0787c68939000ab5f3810388192c5
Gavyn Ezell <ezellgavyn@gmail.com>
2024-10-23 17:17:49 -0700

    sveltekit change

--- src/lib/Skills.ts
+++ src/lib/Skills.ts
@@ -1,6 +1,6 @@
 const skills = {
 	'programming languages': ['Python', 'C++', 'JavaScript', 'PHP', 'SQL', "C#"],
-	'frameworks and libraries': ['FastAPI', 'Express', 'Laravel', 'Svelte', 'PyTorch'],
+	'frameworks and libraries': ['FastAPI', 'Express', 'Laravel', 'SvelteKit', 'PyTorch'],
 	'technologies': ['MySQL', 'MongoDB', 'AWS',  'Git', 'Docker', 'Kubernetes', 'Kafka', 'Unity', 'OpenGL']
 };
 
