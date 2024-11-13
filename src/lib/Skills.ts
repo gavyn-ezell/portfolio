eeebf4dd604267803f8847b45be96a88bd451ab5
Gavyn Ezell <ezellgavyn@gmail.com>
2024-11-13 10:44:43 -0800

    tweaks

--- src/lib/Skills.ts
+++ src/lib/Skills.ts
@@ -1,7 +1,7 @@
 const skills = {
-	'programming languages': ['Python', 'C++', 'JavaScript', 'PHP', 'SQL', "C#"],
-	'frameworks and libraries': ['FastAPI', 'Express', 'Laravel', 'SvelteKit', 'React', 'Next.js', 'PyTorch'],
-	'technologies': ['MySQL', 'MongoDB', 'AWS',  'Git', 'Docker', 'Kubernetes', 'Kafka', 'Unity', 'OpenGL']
+	'programming languages': ['Python', 'C++', 'TypeScript', 'PHP', 'SQL', "C#"],
+	'frameworks and libraries': ['React', 'TailwindCSS', 'Next.js', 'SvelteKit','FastAPI', 'Express', 'Laravel'],
+	'technologies': ['MySQL', 'MongoDB', 'AWS',  'Git', 'Docker', 'Kubernetes', 'Kafka', 'Unity', 'OpenGL'],
 };
 
 export default skills;
